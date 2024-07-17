bfdfc0211e03e2a01c9fd40c3dd58a156c8413f2
Vasu Gupta <vasugupta1313@gmail.com>
2024-07-18 01:25:01 +0530

    OnTap touch area issue fixed

--- lib/features/account/views/account_screen.dart
+++ lib/features/account/views/account_screen.dart
@@ -37,103 +37,115 @@ class AccountScreen extends ConsumerWidget {
             child: Column(
               children: [
                 InkWell(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
                   onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (_)=>ProfileView()));
+                    Navigator.push(context, MaterialPageRoute(builder: (_)=>const ProfileView()));
                   },
                   splashFactory: NoSplash.splashFactory,
-                  child: const Row(
-                    children: [
-                      Icon(Icons.person_outline_outlined),
-                      SizedBox(width: 10,),
-                      Text("Profile",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-                      Expanded(child: SizedBox()),
-                      Icon(Icons.arrow_forward_ios,size: 18,),
-                    ],
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 7),
+                    child: Row(
+                      children: [
+                        Icon(Icons.person_outline_outlined),
+                        SizedBox(width: 10,),
+                        Text("Profile",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
+                        Expanded(child: SizedBox()),
+                        Icon(Icons.arrow_forward_ios,size: 18,),
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10,),
                 Divider(
                   color: Colors.grey.shade300,
                   thickness: 2,
                 ),
-                const SizedBox(height: 10,),
                 InkWell(
                   onTap:() =>Navigator.push(context, MaterialPageRoute(builder: (_)=>const Orders())),
-                  splashFactory: NoSplash.splashFactory,
-                  child: const Row(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 7),
+                    child: Row(
+                      children: [
+                        Icon(Icons.local_shipping_outlined),
+                        SizedBox(width: 10,),
+                        Text("Orders",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
+                        Expanded(child: SizedBox()),
+                        Icon(Icons.arrow_forward_ios,size: 18,),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey.shade300,
+                  thickness: 2,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 7),
+                  child: Row(
                     children: [
-                      Icon(Icons.local_shipping_outlined),
+                      Icon(Icons.headset_mic_outlined),
                       SizedBox(width: 10,),
-                      Text("Orders",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
+                      Text("Help and Support",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
                       Expanded(child: SizedBox()),
                       Icon(Icons.arrow_forward_ios,size: 18,),
                     ],
                   ),
                 ),
-                const SizedBox(height: 10,),
                 Divider(
                   color: Colors.grey.shade300,
                   thickness: 2,
                 ),
-                const SizedBox(height: 10,),
-                const Row(
-                  children: [
-                    Icon(Icons.headset_mic_outlined),
-                    SizedBox(width: 10,),
-                    Text("Help and Support",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-                    Expanded(child: SizedBox()),
-                    Icon(Icons.arrow_forward_ios,size: 18,),
-                  ],
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 7),
+                  child: Row(
+                    children: [
+                      Icon(Icons.lock_outline_rounded),
+                      SizedBox(width: 10,),
+                      Text("Privacy Policy",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
+                      Expanded(child: SizedBox()),
+                      Icon(Icons.arrow_forward_ios,size: 18,),
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 10,),
                 Divider(
                   color: Colors.grey.shade300,
                   thickness: 2,
                 ),
-                const SizedBox(height: 10,),
-                const Row(
-                  children: [
-                    Icon(Icons.lock_outline_rounded),
-                    SizedBox(width: 10,),
-                    Text("Privacy Policy",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-                    Expanded(child: SizedBox()),
-                    Icon(Icons.arrow_forward_ios,size: 18,),
-                  ],
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 7),
+                  child: Row(
+                    children: [
+                      Icon(Icons.help_outline_rounded),
+                      SizedBox(width: 10,),
+                      Text("About",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
+                      Expanded(child: SizedBox()),
+                      Icon(Icons.arrow_forward_ios,size: 18,),
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 10,),
                 Divider(
                   color: Colors.grey.shade300,
                   thickness: 2,
                 ),
-                const SizedBox(height: 10,),
-                const Row(
-                  children: [
-                    Icon(Icons.help_outline_rounded),
-                    SizedBox(width: 10,),
-                    Text("About",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
-                    Expanded(child: SizedBox()),
-                    Icon(Icons.arrow_forward_ios,size: 18,),
-                  ],
-                ),
-                const SizedBox(height: 10,),
-                Divider(
-                  color: Colors.grey.shade300,
-                  thickness: 2,
-                ),
-                const SizedBox(height: 10,),
                 InkWell(
                   onTap: (){
                     ref.read(accountControllerProvider.notifier).logOut(context);
                   },
-                  splashFactory: NoSplash.splashFactory,
-                  child: const Row(
-                    children: [
-                      Icon(Icons.logout_rounded,color: Colors.redAccent,),
-                      SizedBox(width: 10,),
-                      Text("Log out",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500,color: Colors.redAccent),),
-                      Expanded(child: SizedBox()),
-                      Icon(Icons.arrow_forward_ios,size: 18,),
-                    ],
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 7),
+                    child: Row(
+                      children: [
+                        Icon(Icons.logout_rounded,color: Colors.redAccent,),
+                        SizedBox(width: 10,),
+                        Text("Log out",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500,color: Colors.redAccent),),
+                        Expanded(child: SizedBox()),
+                        Icon(Icons.arrow_forward_ios,size: 18,),
+                      ],
+                    ),
                   ),
                 ),
               ],
